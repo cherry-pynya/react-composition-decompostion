57f7a31f47a53b46968f67a1fbc107ada61cabd4
chery-pynya <nikita.cherepnya@gmail.com>
2021-08-09 22:57:50 +0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      /** Родитель для блока новости */
+      <News>
+        /** Виджет который ренлерит новости по категориям переданным в пропс */
+        <ToggleNews categories={['сейчас в сми', 'в германии', 'рекомендуем']}>
+            /** Мапит список новостей по категории */
+            <ToggleNewsLis news={[{}, {}, {}]} activeCategory={'сейчас в сми'} />
+        </ToggleNews>
+        /** Выводит данные с биржи */
+        <NewsStockWidget />
+      </News>
+      /** Родитель для блока работа над ошибками, сделан по принципу карточек в бутсрапе */
+      <SideWidget>
+        {props.children}
+      </SideWidget>
+      /** Родитель для блока поиск */
+      <Search >
+        /** мапит список ссылокб которые переключают на выбранный сервис */
+        <SearchService />
+        /** Флорма поиска */
+        <Form>
+          <Logo />
+          <Input />
+          <button>найти</button>
+        </Form>
+        <SearcExample>
+          <p>Найдется все</p>
+          <SearcExampleLink text={'фаза луны'} />
+        </SearcExample>
+      </Search>
+      /** банер с рекламой */
+      <Banner />
+      /** Родитель для предложенных ссылок*/
+      <Links>
+      /** список из списков */
+        <ul>
+          <li>
+            <LinksWeather />
+          </li>
+          <li>
+            <LinksVisited />
+          </li>
+          <li>
+            <LinksMap />
+          </li>
+          <li>
+            <LinksTV />
+          </li>
+          <li>
+            <LinksStream />
+          </li>
+        </ul>
+      </Links>
     </div>
   );
 }
